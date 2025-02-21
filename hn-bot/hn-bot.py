6e6d84e662ee436aed434deba9b9f6a6f11be77d
boredoms <max@voets.ch>
2025-02-21 17:50:52 +0100

    introduce rate limiting, introduce checking for new posts

--- hn-bot/hn-bot.py
+++ hn-bot/hn-bot.py
@@ -1,8 +1,10 @@
+import asyncio
 from telegram import Update
 import hnapi
 
 from telegram.ext import (
     Application,
+    AIORateLimiter,
     CommandHandler,
     ContextTypes,
     JobQueue,
@@ -20,39 +22,69 @@ def read_token() -> str:
 current_max = 1
 
 
+async def make_post(item_id: str, context: ContextTypes.DEFAULT_TYPE) -> None:
+    story = hnapi.get_item(item_id)
+
+    # only want link posts, ignore ask hn posts
+    if "url" not in story:
+        return
+
+    post_content = f"{story['title']}\n{story['url']}"
+    print("Posting: " + post_content)
+
+    await context.bot.send_message(
+        "@distraction_free_hacker_news",
+        post_content,
+    )
+
+
 async def fetch_posts(context: ContextTypes.DEFAULT_TYPE) -> None:
     global current_max
 
-    if hnapi.get_maxitem() == current_max:
+    print("Checking for new posts")
+    new_max = hnapi.get_maxitem()
+
+    if current_max == new_max:
         return
 
+    print("Fetching posts")
     new_stories = hnapi.get_newstories()
 
-    for id in reversed(new_stories):
-        if id < current_max:
-            continue
+    stories_to_post = filter(lambda x: x > current_max, new_stories)
 
-        story = hnapi.get_item(id)
-        current_max = id
+    print("Queueing up new posts")
+    # queue up the posts so the rate limit is not exceeded
+    context.application.create_task(
+        asyncio.gather(*(make_post(id, context) for id in stories_to_post))
+    )
 
-        post_content = f"{story['title']}\n{story['url']}"
-
-        # TODO: make this post to the channel instead
-        print(story)
-        await context.bot.send_message("@distraction_free_hacker_news", post_content)
+    # update the max post id
+    current_max = new_max
 
 
 def main():
     global current_max
 
+    current_max = hnapi.get_maxitem() - 100
+
     print("Hello from hn-bot!")
     token = read_token()
 
     # set up the tg bot
-    application = Application.builder().token(token).build()
+    application = (
+        Application.builder()
+        .token(token)
+        .rate_limiter(AIORateLimiter(overall_max_rate=1, overall_time_period=3))
+        .build()
+    )
+
     job_queue = application.job_queue
 
-    job_queue.run_repeating(fetch_posts, 10)
+    if job_queue is None:
+        # TODO: error handling
+        return 57
+
+    job_queue.run_repeating(fetch_posts, 2)
 
     # Run the bot until the user presses Ctrl-C
     # We pass 'allowed_updates' handle *all* updates including `chat_member` updates
