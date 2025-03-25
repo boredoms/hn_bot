5e35527b8f0738afb5cd765a4898d4eb140c6021
boredoms <max@voets.ch>
2025-03-25 18:25:32 +0100

    add async api calls

--- hn-bot/apis/tg_api.py
+++ hn-bot/apis/tg_api.py
@@ -1,6 +1,9 @@
 import httpx
+from bot_config import BotConfig
 
 api_url = "https://api.telegram.org"
+
+_async_client = BotConfig.get().async_http_client
 
 
 def send_message(token: str, chat_id: str, text: str):
@@ -9,6 +12,18 @@ def send_message(token: str, chat_id: str, text: str):
     request_url = "/".join([api_url, token, "sendMessage"])
 
     response = httpx.post(
+        request_url, data={"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
+    )
+
+    return response.json()
+
+
+async def async_send_message(token: str, chat_id: str, text: str):
+    token = "bot" + token
+
+    request_url = "/".join([api_url, token, "sendMessage"])
+
+    response = await _async_client.post(
         request_url, data={"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
     )
 
@@ -30,7 +45,24 @@ def edit_message_text(token: str, chat_id: str, message_id: str, text: str):
         },
     )
 
-    print(response.json)
+    return response.json()
+
+
+async def async_edit_message_text(token: str, chat_id: str, message_id: str, text: str):
+    token = "bot" + token
+
+    request_url = "/".join([api_url, token, "editMessageText"])
+
+    response = await _async_client.post(
+        request_url,
+        data={
+            "chat_id": chat_id,
+            "message_id": message_id,
+            "text": text,
+            "parse_mode": "HTML",
+        },
+    )
+
     return response.json()
 
 
@@ -38,11 +70,17 @@ def get_me(token: str):
     token = "bot" + token
     request_url = "/".join([api_url, token, "getMe"])
 
-    print(request_url)
-
     response = httpx.get(request_url)
 
-    print(response.json())
+    return response.json()
+
+
+async def async_get_me(token: str):
+    token = "bot" + token
+    request_url = "/".join([api_url, token, "getMe"])
+
+    response = await _async_client.get(request_url)
+
     return response.json()
 
 
