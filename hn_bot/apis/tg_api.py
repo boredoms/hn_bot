b8390326a5eae1de2357e94150c9b77fb7a53017
boredoms <max@voets.ch>
2025-03-28 11:57:46 +0100

    refactor module and add logging

--- hn_bot/apis/tg_api.py
+++ hn_bot/apis/tg_api.py
@@ -1,6 +1,42 @@
 import httpx
+import json
+import logging
 
 api_url = "https://api.telegram.org"
+
+logger = logging.getLogger(__name__)
+
+
+def make_api_post(request_url: str, data: Any):
+    try:
+        response = httpx.post(request_url, data=data)
+        return response.json()
+    except httpx.HTTPStatusError as e:
+        logger.error(
+            f"error response {e.response.status_code} for request {e.request.url}"
+        )
+    except httpx.RequestError as e:
+        logger.error(f"error making request {e.request.url}")
+    except json.JSONDecodeError as e:
+        logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
+
+    return None
+
+
+def make_api_get(request_url: str):
+    try:
+        response = httpx.get(request_url)
+        return response.json()
+    except httpx.HTTPStatusError as e:
+        logger.error(
+            f"error response {e.response.status_code} for request {e.request.url}"
+        )
+    except httpx.RequestError as e:
+        logger.error(f"error making request {e.request.url}")
+    except json.JSONDecodeError as e:
+        logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
+
+    return None
 
 
 def send_message(token: str, chat_id: str, text: str):
@@ -8,11 +44,9 @@ def send_message(token: str, chat_id: str, text: str):
 
     request_url = "/".join([api_url, token, "sendMessage"])
 
-    response = httpx.post(
-        request_url, data={"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
+    return make_api_post(
+        request_url, {"chat_id": chat_id, "text": text, "parse_mode": "HTML"}
     )
-
-    return response.json()
 
 
 def edit_message_text(token: str, chat_id: str, message_id: str, text: str):
@@ -20,9 +54,9 @@ def edit_message_text(token: str, chat_id: str, message_id: str, text: str):
 
     request_url = "/".join([api_url, token, "editMessageText"])
 
-    response = httpx.post(
+    return make_api_post(
         request_url,
-        data={
+        {
             "chat_id": chat_id,
             "message_id": message_id,
             "text": text,
@@ -30,13 +64,9 @@ def edit_message_text(token: str, chat_id: str, message_id: str, text: str):
         },
     )
 
-    return response.json()
-
 
 def get_me(token: str):
     token = "bot" + token
     request_url = "/".join([api_url, token, "getMe"])
 
-    response = httpx.get(request_url)
-
-    return response.json()
+    return make_api_get(request_url)
