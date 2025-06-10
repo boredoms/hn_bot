045b80b53dc5938eaf0d359c82c65eb73cba1a8d
boredoms <max@voets.ch>
2025-06-10 22:20:11 +0200

    add retry mechanism to telegram api

--- hn_bot/apis/async_apis/tg_api.py
+++ hn_bot/apis/async_apis/tg_api.py
@@ -1,5 +1,8 @@
-import logging
+import asyncio
 import json
+import logging
+import random
+
 import httpx
 
 api_url = "https://api.telegram.org"
@@ -8,26 +11,50 @@ logger = logging.getLogger(__name__)
 
 
 async def make_api_post(request_url: str, data, async_client: httpx.AsyncClient):
-    try:
-        response = await async_client.post(request_url, data=data)
-        return response.json()
-    except json.JSONDecodeError as e:
-        logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
-    except httpx.HTTPError as exc:
-        logger.error(f"HTTP Error - {type(exc)}: {exc}")
+    num_tries = 0
+    sleep_time = 5.0
+    while num_tries < 3:
+        try:
+            response = await async_client.post(request_url, data=data)
+            return response.json()
+        except json.JSONDecodeError as e:
+            logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
+        except httpx.HTTPError as exc:
+            logger.error(f"HTTP Error - {type(exc)}: {exc}")
 
+        sleep_time += random.normalvariate(sigma=num_tries + 1)
+
+        await asyncio.sleep(sleep_time)
+
+        num_tries += 1
+        sleep_time *= 2
+
+    logger.error(
+        f"too many errors for request_url {request_url} with data: {data} - giving up"
+    )
     return None
 
 
 async def make_api_get(request_url: str, async_client: httpx.AsyncClient):
-    try:
-        response = await async_client.get(request_url)
-        return response.json()
-    except json.JSONDecodeError as e:
-        logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
-    except httpx.HTTPError as exc:
-        logger.error(f"HTTP Error - {type(exc)}: {exc}")
+    num_tries = 0
+    sleep_time = 5.0
+    while num_tries < 3:
+        try:
+            response = await async_client.get(request_url)
+            return response.json()
+        except json.JSONDecodeError as e:
+            logger.error(f"error decoding json in response {e.doc} at position {e.pos}")
+        except httpx.HTTPError as exc:
+            logger.error(f"HTTP Error - {type(exc)}: {exc}")
 
+        sleep_time += random.normalvariate(sigma=num_tries + 1)
+
+        await asyncio.sleep(sleep_time)
+
+        num_tries += 1
+        sleep_time *= 2
+
+    logger.error(f"too many errors for request_url {request_url} - giving up")
     return None
 
 
